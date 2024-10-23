9494f790fc50c8df19f649b86010e0bb346a3565
Alejandro <jtzibf@miumg.edu.gt>
2024-10-22 19:06:55 -0600

    avances de la cap 9

--- APP/urls.py
+++ APP/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('',include(('bases.urls','bases'), namespace='bases')),  
+    path('',include(('bases.urls','bases'), namespace='bases')),
+    path('inv/',include(('inv.urls','inv'), namespace='inv')),
+    path('cmp/', include(('cmp.urls', 'cmp'), namespace='cmp')),
     path('admin/', admin.site.urls),
 ]
