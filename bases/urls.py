2559f2bf672b38486ded63cc73919a1114da308f
Alejandro <jtzibf@miumg.edu.gt>
2024-10-27 03:47:27 -0600

    cap 10

--- bases/urls.py
+++ bases/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from bases.views import Home
+from bases.views import Home, HomeSinPrivilegios
 
 app_name = 'bases'
 
@@ -10,4 +10,8 @@ urlpatterns = [
         name='login'),
     path('logout/',auth_views.LoginView.as_view(template_name='bases/login.html'),
         name='logout'),
+
+    path('sin_privilegios/',
+         HomeSinPrivilegios.as_view(),
+         name='sin_privilegios'),
 ]
